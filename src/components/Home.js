d4efc30720b607129556c68e1034576054197fe2
Quba Abdus <quba.abdus13@gmail.com>
2021-09-10 20:43:36 +0530

    Feat:Add stylesheets structure

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,13 @@
-import React from "react";
+import React from 'react';
 
 function Home(props) {
-  return <div>Home</div>;
+  return (
+    <div>
+      Home
+      <div className="home">css test</div>
+      <div className="redText">variables test</div>
+    </div>
+  );
 }
 
 export default Home;
