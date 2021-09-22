5bbe7bf4318694ac6f8796cf198061d56544e48f
Quba Abdus <quba.abdus13@gmail.com>
2021-09-22 17:42:47 +0530

    Feat:Add bootstrap grid

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './scss/index.scss';
 import App from './App';
-// import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/css/bootstrap-grid.min.css';
 
 ReactDOM.render(
   <React.StrictMode>
