65d7238d661f8d39dad46b7b6ba36120399d3357
Quba Abdus <quba.abdus13@gmail.com>
2021-09-23 15:56:16 +0530

    Feat:Add my works section

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,7 +4,11 @@ import Desc from './SharedComponents/Desc/Desc';
 import TitleSecondary from './SharedComponents/Title/TitleSecondary';
 import Card from './SharedComponents/Card/Card';
 import Button from './SharedComponents/Button/Button';
+import hehebot from '../assets/images/hehebot.jpg';
+import laptopDark from '../assets/images/laptop-dark.png';
+import laptopLight from '../assets/images/laptop-light.jpg';
 
+// import "../assets/images/hehebot"
 function Home(props) {
   let introTitle = 'Hi, I am a Software Engineer';
   let aboutMeDesc =
@@ -21,19 +25,18 @@ function Home(props) {
   return (
     <div>
       {/* banner */}
-
       {/* intro-section */}
       <section className="grid-main-container ">
         <section className="grid-container ">
           <div className="row ">
+            {/* <svg>
+              <use href="../assets/svg/icons.svg#main-logo"></use>
+            </svg> */}
+
             <div className="col-12 col-md-6 ">
               <TitleSecondary isLeftLine="true" secondaryTitle="about me" />
               <div className="pt60">
                 <Desc desc={aboutMeDesc} />
-              </div>
-
-              <div className="flexCenter pt60">
-                <Button />
               </div>
             </div>
             <div className="col-12 col-md-6 ">
@@ -44,7 +47,6 @@ function Home(props) {
           </div>
         </section>
       </section>
-
       {/* skills-section */}
       <section className="darkBg mt80 pt80">
         <section className="grid-main-container">
@@ -65,6 +67,49 @@ function Home(props) {
                   <Card key={i} cardTitle={card.cardTitle} />
                 </div>
               ))}
+            </div>
+          </section>
+        </section>
+      </section>
+
+      {/* my work section */}
+      <section className="darkBg mt80 pt80">
+        <section className="grid-main-container">
+          <section className="grid-container ">
+            <div className="row">
+              <div className="col-12">
+                <div className="flexCenter">
+                  <TitleSecondary
+                    isRightLine="true"
+                    isLeftLine="true"
+                    secondaryTitle="my work"
+                  />
+                </div>
+              </div>
+
+              <div className="col-12 col-md-7 pt60">
+                <img alt="hehebot" src={hehebot} />
+              </div>
+              <div className="col-12 col-md-5 pt60">
+                <Title title={'HeheBot'} />
+                <TitleSecondary secondaryTitle="my work" />
+                <Desc desc={aboutMeDesc} />
+              </div>
+
+              <div className="col-12 col-md-3 pt60">
+                <Title title={'HeheBot'} />
+                <Desc desc={aboutMeDesc} />
+              </div>
+              <div className="col-12 col-md-4 pt60">
+                <img alt="hehebot" src={laptopDark} />
+              </div>
+              <div className="col-12 col-md-5 pt60">
+                <img alt="hehebot" src={laptopLight} />
+              </div>
+
+              <div className="flexCenter pt60">
+                <Button />
+              </div>
             </div>
           </section>
         </section>
